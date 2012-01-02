51340aaf542e907e15c91a7b0fc11639ec5deb16
Klaus Ellebracht <klaus.ellebracht@gmail.com>
2012-01-02 11:13:30 +0100

    second commit

---
+++ JustATest/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JustATest::Application.config.secret_token = '5b3ee13c419aaf6c3fc083903628cb11d6638f92f9886cf9796b9ce0045a128988d80f4cec59179fbd8951a7ff7a98d15cc513c46cf004f720a56ccdf51fc3ce'
